08df237a3c71d41935d6f6b5b1297c673e5e6a2a
sivakanagapriyanv2145 <151417218+sivakanagapriyanv2145@users.noreply.github.com>
2025-12-24 10:13:35 +0530

    Create striver pattern15.java

---
+++ striver pattern15.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+  int i,j,n=5;
+  char a='A';
+  for(i=0;i<n;i++){
+      for(j=0;j<n-i;j++){
+          System.out.print(a+" ");
+          a++;
+      }
+      a='A';
+      System.out.println();
+  }
+    }
+}
